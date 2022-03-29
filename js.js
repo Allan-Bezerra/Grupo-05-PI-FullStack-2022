bc1f965ec510e5381583423046cbf3aa2e80aace
desantojohnny <johnny.santo@gmail.com>
2022-03-28 22:54:01 -0300

    btn-hamburger-01

--- js.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile');
-
-function btnMobile() {
-    const menuCentral = document.getElementById('menu-central');
-    menuCentral.classList.toggle('active')
-}
-
-btnMobile.addEventListener('click', toggleMenu);
