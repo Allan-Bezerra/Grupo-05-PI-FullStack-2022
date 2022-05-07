debce7a788c626be2b35208e2590fb6cbc63e122
Allan Bezerra <allanbezerra@protonmail.com>
2022-05-07 12:46:08 -0300

    Atualização do caminho de referência das imagens.

--- projetoIntegrador/app.js
+++ projetoIntegrador/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Estamos apenas começando heheh!</h1>')
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
 app.listen (port, () => {
